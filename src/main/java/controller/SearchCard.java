cf0b5df3df99490499be9b452caa9a302fce9baf
THamilton1123 <thamilton12@madisoncollege.edu>
2026-09-19 16:50:14 -0500

    Created src/main/java/controller directory and SearchCard.java inside that directory, copy/pasted code from coursework, tweaked code to be for cards instead of users.

---
+++ src/main/java/controller/SearchCard.java
@@ -0,0 +1,35 @@
+package controller;
+
+import persistence.CardData;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+/**
+ * A simple servlet to browse a MTG card collection.
+ * @author thamilton12
+ */
+
+@WebServlet(
+        urlPatterns = {"/searchCard"}
+)
+
+public class SearchCard extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        CardData cardData = new CardData();
+        if (req.getParameter("submit").equals("search")) {
+            req.setAttribute("cards", cardData.getCardsByName(req.getParameter("searchTerm")));
+        } else {
+            req.setAttribute("cards", cardData.getAllCards());
+        }
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/results.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
